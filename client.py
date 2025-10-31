0642ef46b3be47f0c54aba96dcdbec12e0c06e77
Tacitus-SL <svyatoslav.lesnikov@gmail.com>
2025-10-31 17:22:10 +0200

    Add client that fails when server not running

--- client.py
+++ client.py
@@ -4,14 +4,14 @@ import sys
 url = 'http://127.0.0.1:8080/'
 try:
     response = requests.get(url)
-    print("Status code: ", response.status_code)
-    print("Response: ", response.text)
 
-    if response.status_code != 200:
-        print("Some error occured.")
+    if response.status_code == 200:
+        print("Server is working correctly!")
+        sys.exit(0)
+    else:
+        print(f"Unexpected response from server: {response.text}")
         sys.exit(1)
-    print("Everything is fine.")
 
 except Exception as e:
-    print("Some error occured.")
-    sys.exit(1)
+    print(f"Client failed to connect to server:{e}")
+    sys.exit(2)
